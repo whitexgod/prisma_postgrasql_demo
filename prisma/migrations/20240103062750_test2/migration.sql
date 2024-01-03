09afe8ee293d3d14c5fc79055323d61b1addaf30
Tuhin Mukherjee <59216687+whitexgod@users.noreply.github.com>
2024-01-03 12:42:42 +0530

    update

---
+++ prisma/migrations/20240103062750_test2/migration.sql
@@ -0,0 +1,29 @@
+/*
+  Warnings:
+
+  - You are about to drop the `UserPreferences` table. If the table is not empty, all the data it contains will be lost.
+  - A unique constraint covering the columns `[userPreferenceId]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- DropForeignKey
+ALTER TABLE "UserPreferences" DROP CONSTRAINT "UserPreferences_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "userPreferenceId" TEXT;
+
+-- DropTable
+DROP TABLE "UserPreferences";
+
+-- CreateTable
+CREATE TABLE "UserPreference" (
+    "id" TEXT NOT NULL,
+    "emailUpdates" BOOLEAN NOT NULL,
+
+    CONSTRAINT "UserPreference_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_userPreferenceId_key" ON "User"("userPreferenceId");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_userPreferenceId_fkey" FOREIGN KEY ("userPreferenceId") REFERENCES "UserPreference"("id") ON DELETE SET NULL ON UPDATE CASCADE;
